58b8381412fcdb76021b54b6094f563b0ad634fd
tyoung17 <tyoung17@users.noreply.github.com>
2016-04-27 22:44:38 -0500

    Populated class and device box after submission
    And fixed white background

--- ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/StudentSubmissionView.java
+++ ObjectLabSoftware/src/ObjectLabEnterpriseSoftware/StudentSubmissionView.java
@@ -245,9 +245,10 @@ public class StudentSubmissionView extends javax.swing.JFrame
         getContentPane().add(idOfUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 50, 130, 30));
 
         jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ObjectLabEnterpriseSoftware/images/white_bg.jpg"))); // NOI18N
-        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 430, 260));
+        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 430, 315));
 
-        editMenu.setText("Help");
+	
+        //editMenu.setText("Help");
 
         jMenuItem1.setText("Contents");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
@@ -335,8 +336,8 @@ public class StudentSubmissionView extends javax.swing.JFrame
         setVisible(false);
         fileLocation.setText(null);
         projName.setText(null);
-        classBox.setSelectedItem(null);
-        printerBox.setSelectedItem(null);
+        //classBox.setSelectedItem(null);
+        //printerBox.setSelectedItem(null);
         setVisible(true);
     }//GEN-LAST:event_Student_SubmitActionPerformed
 
